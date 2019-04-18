de466b8c6860238c4f681a8c1ad55eef88a4da3b
Walney Negreiros Gomes <36850527+walneygomes@users.noreply.github.com>
2019-04-18 16:35:45 -0300

    Update MainAppProgram.java

--- MainAppProgram.java
+++ MainAppProgram.java
@@ -10,7 +10,7 @@ public class MainAppProgram {
 		// valor na circuferencia do circulo
 		System.out.print(Circulo.calculoCircuferencia(raio));
 		// valor do volume da esfera
-		System.out.print(Circulo.calculoCircuferencia(raio));
+		System.out.print(Esfera.calcularVolume(2.3, raio));
 		teclado.close();
 	}
 
